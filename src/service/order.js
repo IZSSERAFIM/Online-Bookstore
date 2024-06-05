c78f061848121b8f4c1e869bc5cfff48e84ddc50
liruobin <king.alex.realmadrid@gmail.com>
2024-06-05 16:22:05 +0800

    chore: Add await to order.js and handle order success and failure messages

--- src/service/order.js
+++ src/service/order.js
@@ -17,8 +17,12 @@ export async function addOrder(order){
     let url = `${BASEURL}/order/add`
     let res
     try{
-        res = post(url, order)
-        message.success("下单成功")
+        res = await post(url, order) // 添加await
+        if (res === true) { // 如果res为true，表示下单成功
+            message.success("下单成功")
+        } else { // 否则，表示下单失败
+            message.error("库存不足，下单失败")
+        }
     }catch(e){
         console.log(e)
         res = {
