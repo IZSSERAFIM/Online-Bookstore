6d779bfa08a90272d425ef30a7b8cc447456aaaf
liruobin <king.alex.realmadrid@gmail.com>
2024-09-26 20:55:24 +0800

    finish timer func(scope session)

--- src/service/logout.js
+++ src/service/logout.js
@@ -1,18 +1,22 @@
 import { BASEURL, post } from "./common";
-import AuthProvider from "./AuthProvider";
+import { message } from "antd";
 
 export const logout = async (username, password) => {
-  // const auth = AuthProvider();
-  // auth.logoutAction();
   localStorage.removeItem("site");
   localStorage.removeItem("user");
   const url = `${BASEURL}/logout?username=${username}&password=${password}`;
   let result;
   try {
-    result = await post(url, { name: username, password: password });
-    console.log("LOGOUT" + result);
+    let response = await post(url, { name: username, password: password });
+    console.log("服务器响应:", response);
+
+    // 假设服务器返回的响应是JSON格式
+    let timeElapsed = response.timeElapsed; // 根据实际返回的JSON结构进行调整
+    console.log("LOGOUT");
+    message.success("登出成功, 在线时间: " + timeElapsed + "ms");
+    result = true;
   } catch (e) {
-    console.log(e);
+    console.log("错误信息:", e);
     result = false;
   }
   return result;
